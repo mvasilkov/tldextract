23a519a1440150148eeaea62bb2f3d31733828e0
Mark Vasilkov <mvasilkov@gmail.com>
2012-01-06 02:15:13 +0200

    Ukraine TLDs

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo '# dyndns' > multisite_names.txt
+# echo '# dyndns' > multisite_names.txt
 bin/dyndns.pl
 
+echo >> multisite_names.txt
 echo '# no-ip' >> multisite_names.txt
 bin/no-ip.pl
